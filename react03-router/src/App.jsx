fb5d40ba1715c85b5f2084482a56b54e820a8971
youngbinko0219 <youngbinko0219@gmail.com>
2024-12-23 12:16:30 +0900

    주석추가

--- react03-router/src/App.jsx
+++ react03-router/src/App.jsx
@@ -98,16 +98,32 @@ const NotFound = () => {
 
 /*
 /intro/router 경로가 요청되었을 때 아웃렛에 렌더링되는 컴포넌트
+useLocation :
+리액트 라우터를 통해 라우팅된 페이지에서 현재 URL과 관련된 정보를 얻는데 사용된다.
+URL경로, 쿼리스트링 등의 관련 정보를 제공한다.
+
+useSearchParams :
+현재 URL의 쿼리스트링을 얻어오거나 조작할 때 사용한다.
 */
 const RouterHooks = () => {
   const location = useLocation();
 
+  // 쿼리스트링의 정보를 얻어오기기 위한 변수와 변경을 위한 함수까지 정의
   const [searchParams, setSearchParams] = useSearchParams();
+  // 쿼리스트링에서 파라미터를 얻어온다.
+  // 첫 진입시에는 둘다 null이 된다.
+  // JSP의 request.getParameter()와 기능적으로 동일하다.
   const mode = searchParams.get("mode");
   const pageNum = searchParams.get("pageNum");
 
+  // 파라미터 mode 값을 토글시켜주는 함수 정의
   const changeMode = () => {
+    // 삼항연산자를 통해 list/view를 토글한다.
     const nextMode = mode === "list" ? "view" : "list";
+    /*
+    파라미터 변경을 위한 setxx()함수를 ㅌㅇ해 값을 변경시킨다.
+    pageNum의 경우 값이 지정되지 않았으므로 기존의 값을 유지한다.
+    */
     setSearchParams({
       mode: nextMode,
       pageNum,
@@ -129,6 +145,7 @@ const RouterHooks = () => {
   const prevPage = () => {
     let pageTemp =
       pageNum === null || isNaN(pageNum) ? 1 : parseInt(pageNum) - 1;
+      // 계산된 페이지 번호가 0이면 1페이지로 고정한다.
     if (pageTemp === 0) {
       pageTemp = 1;
       window.alert("첫번째 페이지 입니다.");
@@ -143,6 +160,8 @@ const RouterHooks = () => {
       <h2>라우터 관련 Hook</h2>
       <div>
         <ul>
+          {/* useLocation 훅을 통해 얻을 수 있는 정보 */}
+          {/* pathname : 쿼리스트링을 제외한 경로까지를 반환 */}
           <li>URL : {location.pathname}</li>
           <li>쿼리스트링 : {location.search}</li>
           <li>mode : {mode}</li>
