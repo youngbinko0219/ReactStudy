0afb0dbfc7f13dcbbcf893b1b6fd49291827c072
youngbinko0219 <youngbinko0219@gmail.com>
2025-01-02 12:33:49 +0900

    주석추가

--- react14-realtime/src/components/ChatMessage.jsx
+++ react14-realtime/src/components/ChatMessage.jsx
@@ -6,21 +6,31 @@ import { useSearchParams } from "react-router-dom";
 import { useRef } from "react";
 import { useEffect } from "react";
 
+// 스크롤바를 최하단으로 내려주기 위한 js 함수
 const scrollTop = (chatWindow) => {
   console.log("scrollTop 호출됨");
   chatWindow.scrollTop = chatWindow.scrollHeight;
 };
 
+// 컴포넌트 정의
 function ChatMessage() {
+  // 쿼리스트링으로 전달된 파라미터를 조작할 때 사용하는 라우터 훅
   const [searchParam, SetSearchParam] = useSearchParams();
+  // 방명과 대화명을 파라미터로 얻어온다.
   const roomId = searchParam.get("roomId");
   const userId = searchParam.get("userId");
+  // 채팅 내역이 보여지는 부분의 DOM 참조
   const chatWindow = useRef();
 
+  // 채팅 저장용 state
   const [chatData, setChatData] = useState("");
 
+  // realtime에 대화내역 저장
   function messageWrite(chatRoom, chatId, chatMessage) {
+    // 고유키 생성
     const newPostKey = push(child(ref(realtime), "tempValue")).key;
+    // 데이터 저장시 '방명'이 최상위 node가 되고 하위에 '고유키'로 구분하여 대화 내용을 저장한다.
+    // 입력된 순서가 지켜지므로 별도의 정렬은 필요없다.
     set(ref(realtime, chatRoom + "/" + newPostKey), {
       id: chatId,
       message: chatMessage,
@@ -28,24 +38,41 @@ function ChatMessage() {
     console.log("입력성공");
   }
 
+  // realtime 리스너 정의
+  // 새롭게 입력된 대화 내용을 실시간으로 얻어온다.
+  // 채팅 내역이 있는 room1 노드를 통해 참조를 생성
   const dbRef = ref(realtime, roomId);
   useEffect(() => {
+    // 리스너 생성
     onValue(dbRef, (snapshot) => {
       let showDiv = [];
       snapshot.forEach((childSnapshot) => {
         const childData = childSnapshot.val();
         console.log("리스너", childData.id, userId);
-        if (childData === userId) {
+        // 대화 내용은 종류에 따라 좌/우측으로 정렬한다.
+        if (childData.id === userId) {
+          // 내가 보낸 메세지라면 '우측'으로 정렬해서 출력한다.
           showDiv.push(
             <div className="myMsg" style={{ textAlign: "right" }}>
+              <strong>{childData.id} :</strong>
+              <br />
               {childData.message}
             </div>
           );
+          // 상대방이 보낸 메세지는 좌측으로 정렬한다.
         } else {
-          showDiv.push(<div>{childData.message}</div>);
+          showDiv.push(
+            <div>
+              <strong>{childData.id} :</strong>
+              <br />
+              {childData.message}
+            </div>
+          );
         }
+        // 스크롤바를 최하단으로 내려준다.
         scrollTop(chatWindow.current);
       });
+      // state를 변경해서 대화 내역을 새롭게 렌더링한다.
       setChatData(showDiv);
     });
   }, []);
@@ -62,6 +89,7 @@ function ChatMessage() {
       >
         채팅종료
       </button>
+      {/* 채팅 내역이 출력되는 부분 */}
       <div id="chatWindow" ref={chatWindow}>
         {chatData}
       </div>
@@ -69,8 +97,10 @@ function ChatMessage() {
         <form
           onSubmit={(e) => {
             e.preventDefault();
+            // 타겟으로 폼값 얻어오기
             let chatRoom = e.target.chatRoom.value;
             let chatId = e.target.chatId.value;
+            // 빈 값에 대한 검증
             if (chatId === "") {
               alert("대화명을 입력하세요");
               return;
@@ -81,7 +111,9 @@ function ChatMessage() {
               return;
             }
             console.log("submit", chatRoom, chatId, message);
+            // 입력함 폼 값을 정리해서 realtime에 입력
             messageWrite(chatRoom, chatId, message);
+            // 입력이 완료되면 <input>을 비워준다.
             e.target.message.value = "";
           }}
         >
--- react14-realtime/src/components/ChatStart.jsx
+++ react14-realtime/src/components/ChatStart.jsx
@@ -2,9 +2,15 @@ import { useRef } from "react";
 import Navi from "../components/Navi";
 
 const ChatStart = () => {
-  const refRoom = useRef();
-  const refId = useRef();
+  // <input> 태그의 DOM을 활용하기 위해 useRef 훅 생성
+  const refRoom = useRef(); // 채팅방의 이름
+  const refId = useRef(); // 접속자의 아이디
 
+  // open()함수를 토앻 채팅창을 팝업으로 오픈한다.
+  // 형식]
+  // open(팝업창의 요청url, 창의 이름, 창의 속성)
+  // 두번째 인수인 창의 이름을 부여하면 새로운 창을 열었을 때 항상 같은 위치에서 오픈된다.
+  // 따라서 창의 이름을 부여하지 않아야 새로운 창을 열 수 있다.
   const openChatWin = () => {
     window.open(
       `/chat/talk?roomId=${refRoom.current.value}&userId=${refId.current.value}`,
@@ -17,6 +23,7 @@ const ChatStart = () => {
     <>
       <Navi />
       <h2>Firebase - Realtime Database Chatting</h2>
+      {/* <input> 태그에 앞에서 생성한 Ref 변수를 추가하여 DOM에 접근한다. */}
       방명 : <input
         type="text"
         name="roomId"
